95340d6736301dc912734eee0274cca66360caad
MiguelSan7 <134112758+MiguelSan7@users.noreply.github.com>
2024-07-15 14:09:04 -0600

    logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  onLogout(): void {
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+  }
 }
